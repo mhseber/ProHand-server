505111f2fbe2c7135779d58ea81fed7fa92c4ae6
mahamudul hasan seber <sebermh@gmail.com>
2024-12-25 15:00:56 +0600

    CRUD Operations

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
-app.use(cors());
+app.use(cors({
+
+}));
 app.use(express.json());
 
 
@@ -30,6 +32,16 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        //job related apis
+        const serviceCollection = client.db('ProHand').collection('pro');
+
+        app.get('/pro', async (req, res) => {
+            const cursor = serviceCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
