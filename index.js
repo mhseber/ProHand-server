85d7da0712203093d9787c6c15b3cd08c3459ea4
mahamudul hasan seber <sebermh@gmail.com>
2025-02-07 17:37:34 +0600

    add code

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         //job related apis
         const serviceCollection = client.db('ProHand').collection('pro');
